7a35706f63ca383c6a91b7494036c13d517db0bb
CandidateExperiment <86597927+CandidateExperiment@users.noreply.github.com>
2021-06-28 13:21:15 +0300

    Тесты

---
+++ src/ComplexCalculator.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace Candidate\Calculator;
+
+use LogicException;
+
+/**
+ * Class ComplexCalculator
+ * @package Candidate\Calculator
+ */
+class ComplexCalculator
+{
+    /**
+     * @var float $real Реальная часть.
+     */
+    public $real;
+
+    /**
+     * @var float $imaginary Мнимая часть.
+     */
+    public $imaginary;
+
+    /**
+     * ComplexCalculator constructor.
+     *
+     * @param float $real      Реальная часть.
+     * @param float $imaginary Мнимая часть.
+     */
+    public function __construct(float $real, float $imaginary)
+    {
+        $this->real = $real;
+        $this->imaginary = $imaginary;
+    }
+
+    /**
+     * Сложение.
+     *
+     * @param mixed $c
+     *
+     * @return ComplexCalculator
+     * @throws LogicException if the argument is not numeric or Complex.
+     */
+    public function add($c): ComplexCalculator
+    {
+        if (is_numeric($c)) {
+            $real = $this->real + $c;
+            $imaginary = $this->imaginary;
+        } elseif ($c instanceof ComplexCalculator) {
+            $real = $this->real + $c->real;
+            $imaginary = $this->imaginary + $c->imaginary;
+        } else {
+            throw new LogicException('Аргумент должен быть числом или экземпляром ComplexCalculator.');
+        }
+
+        return new ComplexCalculator($real, $imaginary);
+    }
+
+    /**
+     * Вычитание.
+     *
+     * @param mixed $c
+     *
+     * @return ComplexCalculator
+     *
+     * @throws LogicException if the argument is not numeric or Complex.
+     */
+    public function subtract($c): ComplexCalculator
+    {
+        if (is_numeric($c)) {
+            $real = $this->real - $c;
+            $imaginary = $this->imaginary;
+        } elseif ($c instanceof ComplexCalculator) {
+            $real = $this->real - $c->real;
+            $imaginary = $this->imaginary - $c->imaginary;
+        } else {
+            throw new LogicException('Аргумент должен быть числом или экземпляром ComplexCalculator.');
+        }
+
+        return new ComplexCalculator($real, $imaginary);
+    }
+
+    /**
+     * Деление.
+     *
+     * @param mixed $c
+     *
+     * @return ComplexCalculator
+     *
+     * @throws LogicException Если аргумент не число или не экземпляр ComplexCalculator.
+     */
+    public function multiply($c): ComplexCalculator
+    {
+        if (is_numeric($c)) {
+            $real = $c * $this->real;
+            $imaginary = $c * $this->imaginary;
+        } elseif ($c instanceof ComplexCalculator) {
+            $real = $this->real * $c->real - $this->imaginary * $c->imaginary;
+            $imaginary = $this->imaginary * $c->real + $this->real * $c->imaginary;
+        } else {
+            throw new LogicException('Аргумент должен быть числом или экземпляром ComplexCalculator.');
+        }
+
+        return new ComplexCalculator($real, $imaginary);
+    }
+
+    /**
+     * Деление.
+     *
+     * @param mixed $c
+     *
+     * @return ComplexCalculator
+     *
+     * @throws LogicException Если аргумент не число или не экземпляр ComplexCalculator.
+     */
+    public function divide($c): ComplexCalculator
+    {
+        if (is_numeric($c)) {
+            $real = $this->real / $c;
+            $imaginary = $this->imaginary / $c;
+            return new ComplexCalculator($real, $imaginary);
+        } elseif ($c instanceof ComplexCalculator) {
+            return $this->multiply($c->inverse());
+        } else {
+            throw new LogicException('Аргумент должен быть числом или экземпляром ComplexCalculator.');
+        }
+    }
+
+    /**
+     * Инверсия.
+     *
+     * @return ComplexCalculator
+     *
+     * @throws LogicException
+     */
+    public function inverse(): ComplexCalculator
+    {
+        if ($this->real == 0 && $this->imaginary == 0) {
+            throw new LogicException('Инверсия невозможна');
+        }
+
+        return $this->complexConjugate()->divide($this->abs() ** 2);
+    }
+
+    /**
+     * Сопряжение.
+     *
+     * @return ComplexCalculator
+     */
+    public function complexConjugate(): ComplexCalculator
+    {
+        return new ComplexCalculator($this->real, -1 * $this->imaginary);
+    }
+
+    /**
+     * The absolute value (magnitude) of a complex number (modulus).
+     *
+     * @return number
+     */
+    public function abs()
+    {
+        return sqrt($this->real ** 2 + $this->imaginary ** 2);
+    }
+
+    /**
+     * Строковое представление.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        if ($this->real == 0 & $this->imaginary == 0) {
+            return '0';
+        } elseif ($this->real == 0) {
+            return "$this->imaginary" . 'i';
+        } elseif ($this->imaginary == 0) {
+            return "$this->real";
+        } elseif ($this->imaginary > 0) {
+            return "$this->real" . ' + ' . "$this->imaginary" . 'i';
+        } else {
+            return "$this->real" . ' - ' . (string) \abs($this->imaginary) . 'i';
+        }
+    }
+}
---
+++ tests/ComplexTest.php
@@ -0,0 +1,552 @@
+<?php
+
+
+namespace Candidate\Tests;
+
+use Candidate\Calculator\ComplexCalculator;
+use LogicException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ComplexTest
+ * @package Candidate\Tests
+ */
+class ComplexTest extends TestCase
+{
+    /**
+     * __get.
+     */
+    public function testGet()
+    {
+        // Given
+        $r       = 1;
+        $i       = 2;
+        $complex = new ComplexCalculator($r, $i);
+
+        // Then
+        $this->assertEquals($r, $complex->real);
+        $this->assertEquals($i, $complex->imaginary);
+    }
+
+    /**
+     * @dataProvider dataProviderForComplexConjugate
+     *
+     * @param        number $real
+     * @param        number $imaginary
+     *
+     * @return void
+     */
+    public function testComplexConjugate($real, $imaginary) : void
+    {
+        $c = new ComplexCalculator($real, $imaginary);
+        $cc = $c->complexConjugate();
+
+        $this->assertEquals($c->real, $cc->real);
+        $this->assertEquals($c->imaginary, -1 * $cc->imaginary);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForComplexConjugate(): array
+    {
+        return [
+            [0, 0],
+            [1, 0],
+            [0, 1],
+            [1, 1],
+            [1, 2],
+            [3, 7],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForAbs
+     * @param number $r
+     * @param number $i
+     * @param number $expected
+     *
+     * @return void
+     */
+    public function testAbs($r, $i, $expected) : void
+    {
+        $c = new ComplexCalculator($r, $i);
+
+        $abs = $c->abs();
+
+        $this->assertEquals($expected, $abs);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForAbs(): array
+    {
+        return [
+            [0, 0, 0],
+            [1, 0, 1],
+            [0, 1, 1],
+            [1, 2, \sqrt(5)],
+            [2, 1, \sqrt(5)],
+            [2, 2, \sqrt(8)],
+            [-1, 0, 1],
+            [0, -1, 1],
+            [-1, 2, \sqrt(5)],
+            [2, -1, \sqrt(5)],
+            [-2, -2, \sqrt(8)],
+        ];
+    }
+
+    /**
+     *
+     * Сложение.
+     *
+     * @dataProvider dataProviderForAdd
+     * @param        array  $complex1
+     * @param        array  $complex2
+     * @param        array  $expected
+     */
+    public function testAdd(array $complex1, array $complex2, array $expected) : void
+    {
+        $c1 = new ComplexCalculator($complex1['r'], $complex1['i']);
+        $c2 = new ComplexCalculator($complex2['r'], $complex2['i']);
+
+        $result = $c1->add($c2);
+
+        $this->assertEquals($expected['r'], $result->real);
+        $this->assertEquals($expected['i'], $result->imaginary);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForAdd(): array
+    {
+        return [
+            [
+                ['r' => 3, 'i' => 2],
+                ['r' => 4, 'i' => -3],
+                ['r' => 7, 'i' => -1],
+            ],
+            [
+                ['r' => 0, 'i' => 0],
+                ['r' => 4, 'i' => -3],
+                ['r' => 4, 'i' => -3],
+            ],
+            [
+                ['r' => -3, 'i' => -2],
+                ['r' => 4, 'i' => 3],
+                ['r' => 1, 'i' => 1],
+            ],
+            [
+                ['r' => 7, 'i' => 6],
+                ['r' => 4, 'i' => 4],
+                ['r' => 11, 'i' => 10],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForAddReal
+     *
+     * @param $complex
+     * @param $real
+     * @param $expected
+     *
+     * @return void
+     */
+    public function testAddReal($complex, $real, $expected) : void
+    {
+        $c = new ComplexCalculator($complex['r'], $complex['i']);
+
+        $result = $c->add($real);
+
+        $this->assertEquals($expected['r'], $result->real);
+        $this->assertEquals($expected['i'], $result->imaginary);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForAddReal() : array
+    {
+        return [
+            [
+                ['r' => 3, 'i' => 2],
+                5,
+                ['r' => 8, 'i' => 2],
+            ],
+            [
+                ['r' => 0, 'i' => 0],
+                5,
+                ['r' => 5, 'i' => 0],
+            ],
+            [
+                ['r' => 3, 'i' => 2],
+                -2,
+                ['r' => 1, 'i' => 2],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForSubtract
+     * @param        array  $complex1
+     * @param        array  $complex2
+     * @param        array  $expected
+     *
+     * @return void
+     */
+    public function testSubtract(array $complex1, array $complex2, array $expected) : void
+    {
+        $c1 = new ComplexCalculator($complex1['r'], $complex1['i']);
+        $c2 = new ComplexCalculator($complex2['r'], $complex2['i']);
+
+        $result = $c1->subtract($c2);
+
+        $this->assertEquals($expected['r'], $result->real);
+        $this->assertEquals($expected['i'], $result->imaginary);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForSubtract(): array
+    {
+        return [
+            [
+                ['r' => 3, 'i' => 2],
+                ['r' => 4, 'i' => -3],
+                ['r' => -1, 'i' => 5],
+            ],
+            [
+                ['r' => 3, 'i' => 2],
+                ['r' => 4, 'i' => -3],
+                ['r' => -1, 'i' => 5],
+            ],
+            [
+                ['r' => 0, 'i' => 0],
+                ['r' => 4, 'i' => -3],
+                ['r' => -4, 'i' => 3],
+            ],
+            [
+                ['r' => -3, 'i' => -2],
+                ['r' => 4, 'i' => 3],
+                ['r' => -7, 'i' => -5],
+            ],
+            [
+                ['r' => 7, 'i' => 6],
+                ['r' => 4, 'i' => 4],
+                ['r' => 3, 'i' => 2],
+            ],
+        ];
+    }
+
+
+    /**
+     * @dataProvider dataProviderForSubtractReal
+     * @param $complex
+     * @param $real
+     * @param $expected
+     *
+     * @return void
+     */
+    public function testSubtractReal($complex, $real, $expected) : void
+    {
+        $c = new ComplexCalculator($complex['r'], $complex['i']);
+
+        $result = $c->subtract($real);
+
+        $this->assertEquals($expected['r'], $result->real);
+        $this->assertEquals($expected['i'], $result->imaginary);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForSubtractReal(): array
+    {
+        return [
+            [
+                ['r' => 3, 'i' => 2],
+                5,
+                ['r' => -2, 'i' => 2],
+            ],
+            [
+                ['r' => 0, 'i' => 0],
+                5,
+                ['r' => -5, 'i' => 0],
+            ],
+            [
+                ['r' => 3, 'i' => 2],
+                -2,
+                ['r' => 5, 'i' => 2],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForMultiply
+     * @param        array  $complex1
+     * @param        array  $complex2
+     * @param        array  $expected
+     *
+     * @return void
+     */
+    public function testMultiply(array $complex1, array $complex2, array $expected) : void
+    {
+        $c1 = new ComplexCalculator($complex1['r'], $complex1['i']);
+        $c2 = new ComplexCalculator($complex2['r'], $complex2['i']);
+
+        $result = $c1->multiply($c2);
+
+        $this->assertEquals($expected['r'], $result->real);
+        $this->assertEquals($expected['i'], $result->imaginary);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForMultiply(): array
+    {
+        return [
+            [
+                ['r' => 3, 'i' => 2],
+                ['r' => 1, 'i' => 4],
+                ['r' => -5, 'i' => 14],
+            ],
+            [
+                ['r' => 3, 'i' => 13],
+                ['r' => 7, 'i' => 17],
+                ['r' => -200, 'i' => 142],
+            ],
+            [
+                ['r' => 6, 'i' => 8],
+                ['r' => 4, 'i' => -9],
+                ['r' => 96, 'i' => -22],
+            ],
+            [
+                ['r' => -56, 'i' => 3],
+                ['r' => -84, 'i' => -4],
+                ['r' => 4716, 'i' => -28],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForMultiplyReal
+     * @param $complex
+     * @param $real
+     * @param $expected
+     *
+     * @return void
+     */
+    public function testMultiplyReal($complex, $real, $expected) : void
+    {
+        $c = new ComplexCalculator($complex['r'], $complex['i']);
+
+        $result = $c->multiply($real);
+
+        $this->assertEquals($expected['r'], $result->real);
+        $this->assertEquals($expected['i'], $result->imaginary);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForMultiplyReal() : array
+    {
+        return [
+            [
+                ['r' => 3, 'i' => 1],
+                2,
+                ['r' => 6, 'i' => 2],
+            ],
+            [
+                ['r' => 30, 'i' => 13],
+                2,
+                ['r' => 60, 'i' => 26],
+            ],
+        ];
+    }
+
+    /**
+     * Деление.
+     *
+     * @dataProvider dataProviderForDivide
+     * @param        array  $complex1
+     * @param        array  $complex2
+     * @param        array  $expected
+     *
+     * @return void
+     */
+    public function testDivide(array $complex1, array $complex2, array $expected) : void
+    {
+        $c1 = new ComplexCalculator($complex1['r'], $complex1['i']);
+        $c2 = new ComplexCalculator($complex2['r'], $complex2['i']);
+
+        $result = $c1->divide($c2);
+
+        $this->assertEquals($expected['r'], $result->real);
+        $this->assertEquals($expected['i'], $result->imaginary);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForDivide(): array
+    {
+        return [
+            [
+                ['r' => 3, 'i' => 2],
+                ['r' => 4, 'i' => -3],
+                ['r' => 0.24, 'i' => 0.68],
+            ],
+            [
+                ['r' => 5, 'i' => 5],
+                ['r' => 6, 'i' => 2],
+                ['r' => 1, 'i' => 1 / 2],
+            ],
+            [
+                ['r' => 6, 'i' => 2],
+                ['r' => 7, 'i' => -7],
+                ['r' => 2 / 7, 'i' => 4 / 7],
+            ],
+            [
+                ['r' => -56, 'i' => 3],
+                ['r' => -84, 'i' => -4],
+                ['r' => 69 / 104, 'i' => -7 / 104],
+            ],
+        ];
+    }
+
+    /**
+     * Деление.
+     *
+     * @dataProvider dataProviderForDivideReal
+     * @param $complex
+     * @param $real
+     * @param $expected
+     *
+     * @return void
+     */
+    public function testDivideReal($complex, $real, $expected) : void
+    {
+        $c = new ComplexCalculator($complex['r'], $complex['i']);
+
+        $result = $c->divide($real);
+
+        $this->assertEquals($expected['r'], $result->real);
+        $this->assertEquals($expected['i'], $result->imaginary);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForDivideReal() : array
+    {
+        return [
+            [
+                ['r' => 4, 'i' => 1],
+                2,
+                ['r' => 2, 'i' => 1 / 2],
+            ],
+            [
+                ['r' => 60, 'i' => 9],
+                3,
+                ['r' => 20, 'i' => 3],
+            ],
+        ];
+    }
+
+    /**
+     * Неверный формат аргументов сложения.
+     *
+     * @return void
+     */
+    public function testComplexAddException() : void
+    {
+        $complex = new ComplexCalculator(1, 1);
+
+        $this->expectException(LogicException::class);
+
+        $complex->add("string");
+    }
+
+    /**
+     * Неверный формат аргументов вычитания.
+     *
+     * @return void
+     */
+    public function testComplexSubtractException() : void
+    {
+        $complex = new ComplexCalculator(1, 1);
+
+        $this->expectException(LogicException::class);
+
+        $complex->add("string");
+    }
+
+    /**
+     * Неверный формат аргументов деления.
+     *
+     * @return void
+     */
+    public function testComplexMultiplyException() : void
+    {
+        $complex = new ComplexCalculator(1, 1);
+
+        $this->expectException(LogicException::class);
+
+        $complex->add("string");
+    }
+
+    /**
+     * @dataProvider dataProviderForInverse
+     * @param        number $r
+     * @param        number $i
+     * @param        number $expected_r
+     * @param        number $expected_i
+     *
+     * @return void
+     */
+    public function testInverse($r, $i, $expected_r, $expected_i) : void
+    {
+        $c = new ComplexCalculator($r, $i);
+
+        $inverse = $c->inverse();
+
+        $this->assertEquals($expected_r, $inverse->real);
+        $this->assertEquals($expected_i, $inverse->imaginary);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForInverse(): array
+    {
+        return [
+            [1, 0, 1, 0],
+            [0, 1, 0, -1],
+            [1, 1, 1 / 2, -1 / 2],
+            [4, 6, 1 / 13, -3 / 26],
+            [-4, 6, -1 / 13, -3 / 26],
+            [4, -6, 1 / 13, 3 / 26],
+            [-4, -6, -1 / 13, 3 / 26],
+        ];
+    }
+
+    /**
+     * Неверный формат аргументов инверсии.
+     *
+     * @return void
+     */
+    public function testInverseException()
+    {
+        $complex = new ComplexCalculator(0, 0);
+
+        $this->expectException(LogicException::class);
+
+        $complex->inverse();
+    }
+}
